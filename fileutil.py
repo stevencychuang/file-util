17f9a5bb209bdd549004d1f7221e16641dbb81c8
stevencychuang <gilrstudnet@gmail.com>
2021-04-24 08:39:48 +0800

    Add get_dict_inv and refactor cp2dstex

--- fileutil.py
+++ fileutil.py
@@ -20,9 +20,10 @@ def cp2dst(src, dst):
 
 class FileParser:
 
-    def __init__(self, root, keyword):
+    def __init__(self, root, keyword, rename=""):
         self.root = root
         self.keyword = keyword
+        self.rename = rename
         self._dict = None
         self._path = Path(root)
 
@@ -30,8 +31,12 @@ class FileParser:
         if self._dict is None:
             self._dict = {}
             for path in self._path.rglob(self.keyword):
-                self._dict[get_real_path(str(path))] = path.name
+                self._dict[get_real_path(str(path))] = path.name.replace(self.keyword.replace("*", ""), self.rename)
         return self._dict
+
+    def get_dict_inv(self):
+        _dict = self.get_dict()
+        return dict(zip(_dict.values(), _dict.keys()))
 
     def get_list(self):
         return list(self.get_dict().keys())
@@ -46,10 +51,20 @@ class FileParser:
             list_dst.append(cp2dst(src, dst))
         return list_dst
 
-    def cp2updst(self, root_dst, up=0):
+    def cp2dstex(self, root_dst: str, up=0, replace: dict = None):
+        """!
+        The extension of cp2dst with up levels of destination directory and replace with keyword strings.
+        @param root_dst: the root of destination
+        @param up: the up level of destination directory
+        @param replace: the tuple of the strings (source, target)
+        """
         list_dst = []
-        for src, name in self.get_dict().items():
+        for src in self.get_dict().keys():
             dst = str(src).replace(self.root, root_dst)
             dir_dst = Path(dst).parents[up]
-            list_dst.append(cp2dst(src, dir_dst / name))
+            name_dst = Path(dst).name
+            if isinstance(replace, dict):
+                for k, v in replace.items():
+                    name_dst.replace(k, v)
+            list_dst.append(cp2dst(src, dir_dst / name_dst))
         return list_dst
